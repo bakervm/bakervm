3d4c8f9cc81e9b71b60de868399fbb04b38a575f
Julian Laubstein <contact@julianlaubstein.de>
2016-12-11 22:14:19 +0100

    Added mul and div handling

--- src/vm.rs
+++ src/vm.rs
@@ -26,13 +26,15 @@ impl VM {
         let mut image_string = String::new();
         image_file.read_to_string(&mut image_string).chain_err(|| "unable to read image")?;
 
-        let mut byte_iter = image_string.bytes();
-        let mut outer_byte = byte_iter.next();
-        while let Some(byte) = outer_byte {
+        let byte_iter: Vec<u8> = image_string.bytes().collect();
+        let mut byte = byte_iter[self.ip];
+        while self.ip < byte_iter.len() {
             match byte {
                 bytecode::HALT => break,
                 bytecode::ADD => self.add().chain_err(|| "unable to execute 'add' instruction")?,
                 bytecode::SUB => self.sub().chain_err(|| "unable to execute 'sub' instruction")?,
+                bytecode::MUL => self.mul().chain_err(|| "unable to execute 'mul' instruction")?,
+                bytecode::DIV => self.div().chain_err(|| "unable to execute 'div' instruction")?,
                 bytecode::PRINT => {
                     self.print().chain_err(|| "unable to execute 'print' instruction")?
                 }
@@ -41,15 +43,31 @@ impl VM {
                     let mut res: u32 = 0;
                     for _ in 0..4 {
                         res <<= 8;
-                        res |= byte_iter.next().unwrap() as u32;
+                        self.ip += 1;
+                        res |= byte_iter[self.ip] as u32;
                     }
 
                     self.push(res).chain_err(|| "unable to push value to the stack")?;
                 }
+                bytecode::JMP => {
+                    // Build a u32 from single bytes
+                    let mut res: u32 = 0;
+                    for _ in 0..4 {
+                        res <<= 8;
+                        self.ip += 1;
+                        res |= byte_iter[self.ip] as u32;
+                    }
+
+                    self.jmp(res).chain_err(|| "unable to jump")?;
+                    byte = byte_iter[self.ip];
+                    continue;
+                }
                 _ => bail!("unexpected opcode: {:08x}", byte),
             }
 
-            outer_byte = byte_iter.next();
+            self.ip += 1;
+
+            byte = byte_iter[self.ip];
         }
 
         Ok(())
@@ -89,6 +107,27 @@ impl VM {
         let b = self.pop().chain_err(|| "unable to pop value off the stack")?;
         let a = self.pop().chain_err(|| "unable to pop value off the stack")?;
         self.push((a - b) as u32).chain_err(|| "unable to push to stack")?;
+        Ok(())
+    }
+
+
+    fn mul(&mut self) -> VMResult<()> {
+        let b = self.pop().chain_err(|| "unable to pop value off the stack")?;
+        let a = self.pop().chain_err(|| "unable to pop value off the stack")?;
+        self.push((a * b) as u32).chain_err(|| "unable to push to stack")?;
+        Ok(())
+    }
+
+
+    fn div(&mut self) -> VMResult<()> {
+        let b = self.pop().chain_err(|| "unable to pop value off the stack")?;
+        let a = self.pop().chain_err(|| "unable to pop value off the stack")?;
+        self.push((a / b) as u32).chain_err(|| "unable to push to stack")?;
+        Ok(())
+    }
+
+    fn jmp(&mut self, addr: u32) -> VMResult<()> {
+        self.ip = addr as usize;
         Ok(())
     }
 
