0387497d6e2d0d1f80130108928625cdf7a569e3
Julian Laubstein <contact@julianlaubstein.de>
2017-05-06 13:37:50 +0200

    Added CALL and RET

--- bakervm/vm/mod.rs
+++ bakervm/vm/mod.rs
@@ -12,7 +12,8 @@ use std::path::Path;
 /// The whole state of the VM
 pub struct VM {
     image: Image,
-    stack: Stack,
+    data_stack: Stack,
+    ret_addr_stack: Stack,
     inter_reg: HashMap<Word, Address>,
 }
 
@@ -20,7 +21,8 @@ impl VM {
     pub fn new() -> VM {
         VM {
             image: Image::default(),
-            stack: Stack::default(),
+            data_stack: Stack::default(),
+            ret_addr_stack: Stack::default(),
             inter_reg: HashMap::new(),
         }
     }
@@ -36,21 +38,23 @@ impl VM {
             match byte {
                 bytecode::HALT => break,
                 bytecode::ADD => {
-                    self.stack.add().chain_err(|| "unable to execute 'add' instruction")?
+                    self.data_stack.add().chain_err(|| "unable to execute 'add' instruction")?
                 }
                 bytecode::SUB => {
-                    self.stack.sub().chain_err(|| "unable to execute 'sub' instruction")?
+                    self.data_stack.sub().chain_err(|| "unable to execute 'sub' instruction")?
                 }
                 bytecode::MUL => {
-                    self.stack.mul().chain_err(|| "unable to execute 'mul' instruction")?
+                    self.data_stack.mul().chain_err(|| "unable to execute 'mul' instruction")?
                 }
                 bytecode::DIV => {
-                    self.stack.div().chain_err(|| "unable to execute 'div' instruction")?
+                    self.data_stack.div().chain_err(|| "unable to execute 'div' instruction")?
                 }
                 bytecode::PUSH => {
                     let res = self.image.read_next().chain_err(|| "unable to read word")?;
 
-                    self.stack.push_word(res).chain_err(|| "unable to push value to the stack")?;
+                    self.data_stack
+                        .push_word(res)
+                        .chain_err(|| "unable to push value to the data stack")?;
                 }
                 bytecode::JMP => {
                     let addr: Word = self.image.read_next().chain_err(|| "unable to read word")?;
@@ -61,9 +65,10 @@ impl VM {
                 bytecode::JZ => {
                     let addr: Word = self.image.read_next().chain_err(|| "unable to read word")?;
 
-                    let top_of_stack = self.stack
-                        .peek_number()
-                        .chain_err(|| "unable to get current top of stack")?;
+                    let top_of_stack =
+                        self.data_stack
+                            .peek_number()
+                            .chain_err(|| "unable to get current top of data stack")?;
 
                     if top_of_stack == 0.0 {
                         self.image.jmp(addr as Address);
@@ -73,9 +78,10 @@ impl VM {
                 bytecode::JNZ => {
                     let addr: Word = self.image.read_next().chain_err(|| "unable to read word")?;
 
-                    let top_of_stack = self.stack
-                        .peek_number()
-                        .chain_err(|| "unable to get current top of stack")?;
+                    let top_of_stack =
+                        self.data_stack
+                            .peek_number()
+                            .chain_err(|| "unable to get current top of data stack")?;
 
                     if top_of_stack != 0.0 {
                         self.image.jmp(addr as Address);
@@ -93,6 +99,26 @@ impl VM {
 
             self.image.advance_pc();
         }
+
+        Ok(())
+    }
+
+    fn call(&mut self, address: Address) -> VMResult<()> {
+        self.ret_addr_stack
+            .push_word(address as Word)
+            .chain_err(|| "unable to push address to address stack")?;
+
+        self.image.jmp(address);
+
+        Ok(())
+    }
+
+    fn ret(&mut self) -> VMResult<()> {
+        let return_addr = self.ret_addr_stack
+            .pop_word()
+            .chain_err(|| "unable to pop address off the address stack")?;
+
+        self.image.jmp(return_addr as Address);
 
         Ok(())
     }
--- definitions/bytecode.rs
+++ definitions/bytecode.rs
@@ -5,39 +5,40 @@ pub const PREAMBLE: &str = "BAKER1";
 /// Stops the program and shuts down the vm
 pub const HALT: Byte = 0x01;
 
-/// Adds the two top most values and pushes the result back to the stack. This
-/// also removes the processed values from the stack.
+/// Adds the two top most values and pushes the result back to the data stack.
+/// This also removes the processed values from the data stack.
 pub const ADD: Byte = 0x02;
 
-/// Subtracts the first value on the stack from the second and pushes the
-/// result back to the stack. This also removes the processed values from the
-/// stack.
+/// Subtracts the first value on the data stack from the second and pushes the
+/// result back to the data stack. This also removes the processed values from
+/// the data stack.
 pub const SUB: Byte = 0x03;
 
-/// Adds the two top most values and pushes the result back to the stack. This
-/// also removes the processed values from the stack.
+/// Adds the two top most values and pushes the result back to the data stack.
+/// This also removes the processed values from the data stack.
 pub const MUL: Byte = 0x04;
 
-/// Divides the second value on the stack throuh the first one and pushes the
-/// result back to the top of the stack. This also removes the processed values
-/// from the stack.
+/// Divides the second value on the data stack throuh the first one and pushes
+/// the result back to the top of the data stack. This also removes the
+/// processed values from the data stack.
 pub const DIV: Byte = 0x05;
 
-/// Prints out the top most value on the stack
+/// Prints out the top most value on the data stack
 #[deprecated(since="0.1.0", note="will be replaced soon")]
 pub const PRINT: Byte = 0x06;
 
-/// Pushes a value (Word) to the stack
+/// Pushes a value (Word) to the data stack
 pub const PUSH: Byte = 0x07;
 
 /// An unconditional jump to the specified address
 pub const JMP: Byte = 0x08;
 
-/// Jumps to the specified address if the top most value on the stack equals 0
+/// Jumps to the specified address if the top most value on the data stack
+/// equals 0
 pub const JZ: Byte = 0x09;
 
-/// Jumps to the specified address if the top most value on the stack is not
-/// equal to 0
+/// Jumps to the specified address if the top most value on the data stack is
+/// not equal to 0
 pub const JNZ: Byte = 0x0A;
 
 /// Moves the specified value (Word) to the specified register (Address)
@@ -56,9 +57,16 @@ pub const MOVB: Byte = 0x0E;
 /// specified buffer register (Address)
 pub const MOV: Byte = 0x0F;
 
-/// Moves the top most value of the stack to the specified register (Address)
+/// Moves the top most value of the data stack to the specified register
+/// (Address)
 pub const SMOV: Byte = 0x10;
 
 /// Registers an interrupt callback by mapping the specified signal_id to the
 /// given function call address
 pub const REGI: Byte = 0x11;
+
+/// Calls a function while pushing the return address to the return stack
+pub const CALL: Byte = 0x12;
+
+/// Returns from a function. The return value is pushed to the data stack
+pub const RET: Byte = 0x13;
