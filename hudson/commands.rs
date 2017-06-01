f09704945a470f6e42cd93b52ea78bfdd4281fed
Julian Laubstein <contact@julianlaubstein.de>
2017-06-01 12:50:18 +0200

    Rewrote toolkit and added stock img

---
+++ hudson/commands.rs
@@ -0,0 +1,89 @@
+use clap::ArgMatches;
+use definitions::{DisplayResolution, InternalInterrupt, Target, Value};
+use definitions::ImageBuilder;
+use error::*;
+use std::fs::File;
+use std::io::Write;
+
+pub fn stock(_matches: &ArgMatches) -> CompilationResult<()> {
+    let mut builder = ImageBuilder::new();
+
+    let res_def = DisplayResolution::default();
+
+    let max = res_def.width * res_def.height;
+
+    for x in 0..max {
+        builder.push(Target::Framebuffer(x), Value::Color(0, 0, 0));
+    }
+
+    // Triangle
+    builder.push(
+        Target::Framebuffer(res_def.width + 1),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+    builder.push(
+        Target::Framebuffer((res_def.width * 2) + 2),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+    builder.push(
+        Target::Framebuffer((res_def.width * 3) + 3),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+    builder.push(
+        Target::Framebuffer((res_def.width * 4) + 4),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+    builder.push(
+        Target::Framebuffer((res_def.width * 5) + 3),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+    builder.push(
+        Target::Framebuffer((res_def.width * 6) + 2),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+    builder.push(
+        Target::Framebuffer((res_def.width * 7) + 1),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+
+    // Underscore
+    builder.push(
+        Target::Framebuffer((res_def.width * 7) + 5),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+    builder.push(
+        Target::Framebuffer((res_def.width * 7) + 6),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+    builder.push(
+        Target::Framebuffer((res_def.width * 7) + 7),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+    builder.push(
+        Target::Framebuffer((res_def.width * 7) + 8),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+    builder.push(
+        Target::Framebuffer((res_def.width * 7) + 9),
+        Value::Color(0xFF, 0xFF, 0xFF),
+    );
+
+    builder.int(InternalInterrupt::FlushFramebuffer);
+
+    builder.pause();
+    builder.jmp(max + 1);
+
+    let program_data = builder.gen();
+
+    let mut output_file = File::create("stock.img").chain_err(|| "unable to open output file")?;
+
+    output_file.write_all(&program_data[..]).chain_err(|| "unable to write output file")?;
+
+    output_file.sync_all().chain_err(|| "unable to sync output file to file system")?;
+
+    Ok(())
+}
+
+pub fn build(_matches: &ArgMatches) -> CompilationResult<()> {
+    unimplemented!()
+}
---
+++ hudson/main.rs
@@ -0,0 +1,56 @@
+#[macro_use]
+extern crate error_chain;
+extern crate clap;
+extern crate definitions;
+extern crate bincode;
+
+mod error;
+mod commands;
+
+use clap::{App, AppSettings, Arg, SubCommand};
+use error::*;
+
+fn main() {
+    if let Err(ref e) = run() {
+        println!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> CompilationResult<()> {
+    let matches = App::new("hudson")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Julian Laubstein <contact@julianlaubstein.de>")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .about("The bakervm toolkit")
+        .subcommand(SubCommand::with_name("stock").about("Generate the default image"),)
+        .subcommand(
+            SubCommand::with_name("build")
+                .arg(Arg::with_name("input").index(1).required(true).help("Sets the source file to use."),)
+                .arg(Arg::with_name("output")
+                .takes_value(true)
+                .value_name("FILE")
+                .help("Sets the destination file.")),
+        )
+        .get_matches();
+
+
+    match matches.subcommand() {
+        ("build", Some(sub_match)) => commands::build(sub_match)?,
+        ("stock", Some(sub_match)) => commands::stock(sub_match)?,
+        _ => {}
+    }
+
+    Ok(())
+}
