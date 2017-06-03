6f955539dbe9a4a136c284e3ba8019b786d1d76c
Julian Laubstein <contact@julianlaubstein.de>
2017-06-04 00:08:05 +0200

    Added regular expressions for literals and tests

--- hudson/main.rs
+++ hudson/main.rs
@@ -3,9 +3,13 @@ extern crate error_chain;
 extern crate clap;
 extern crate definitions;
 extern crate bincode;
+extern crate regex;
+#[macro_use]
+extern crate lazy_static;
 
 mod error;
 mod commands;
+mod basm;
 
 use clap::{App, AppSettings, Arg, SubCommand};
 use error::*;
