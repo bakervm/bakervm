c66c98a49a25fc0fdddc8c8a285616d9cbf90a75
Julian Laubstein <contact@julianlaubstein.de>
2017-06-04 11:05:54 +0200

    implemented FromStr for Value

--- definitions/value.rs
+++ definitions/value.rs
@@ -1,7 +1,17 @@
 //! The value and type definitions
 
+use regex::Regex;
 use std::ops::{Add, Div, Mul, Rem, Sub};
+use std::str::FromStr;
 use typedef::*;
+
+lazy_static! {
+    static ref BOOLEAN_RE: Regex = Regex::new("^true|false$").unwrap();
+    static ref FLOAT_RE: Regex = Regex::new("^(-?\\d+)?\\.[0-9]+$").unwrap();
+    static ref INTEGER_RE: Regex = Regex::new("^(-?\\d+)?$").unwrap();
+    static ref COLOR_RE: Regex = Regex::new("^#([0-9abcdefABCDEF]{6})$").unwrap();
+    static ref CHAR_RE: Regex = Regex::new("^'(.)'$").unwrap();
+}
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd)]
 pub enum Type {
@@ -112,6 +122,34 @@ impl Value {
     }
 }
 
+impl FromStr for Value {
+    type Err = Result<Self, &'static str>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if BOOLEAN_RE.is_match(s) {
+            Ok(Value::Boolean(s.parse().unwrap()))
+        } else if FLOAT_RE.is_match(s) {
+            Ok(Value::Float(s.parse().unwrap()))
+        } else if INTEGER_RE.is_match(s) {
+            Ok(Value::Integer(s.parse().unwrap()))
+        } else if COLOR_RE.is_match(s) {
+            let color = COLOR_RE.captures_iter(s).next().unwrap();
+            let uint: u32 = u32::from_str_radix(&color[1], 16).unwrap();
+            let r = (uint >> 16) as u8;
+            let g = (uint >> 8) as u8;
+            let b = uint as u8;
+
+            Ok(Value::Color(r, g, b))
+        } else if CHAR_RE.is_match(s) {
+            let character = CHAR_RE.captures_iter(s).next().unwrap();
+            let real_char: char = character[1].chars().next().unwrap();
+            Ok(Value::Char(real_char))
+        } else {
+            Ok(Value::Undefined)
+        }
+    }
+}
+
 impl Add for Value {
     type Output = Value;
 
@@ -182,6 +220,138 @@ impl Rem for Value {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn boolean_regex() {
+        let input_boolean = "true";
+
+        if !BOOLEAN_RE.is_match(input_boolean) {
+            panic!("input doesn't match a boolean");
+        } else {
+            let boolean: bool = input_boolean.parse().unwrap();
+            assert_eq!(boolean, true);
+        }
+
+        let input_boolean = "false";
+
+        if !BOOLEAN_RE.is_match(input_boolean) {
+            panic!("input doesn't match a boolean");
+        } else {
+            let boolean: bool = input_boolean.parse().unwrap();
+            assert_eq!(boolean, false);
+        }
+    }
+
+    #[test]
+    fn float_regex() {
+        let input_float = "23.123412";
+
+        if !FLOAT_RE.is_match(input_float) {
+            panic!("input doesn't match a float");
+        } else {
+            let float: Float = input_float.parse().unwrap();
+            assert_eq!(float, 23.123412);
+        }
+
+        let input_float = "-23.123412";
+
+        if !FLOAT_RE.is_match(input_float) {
+            panic!("input doesn't match a float");
+        } else {
+            let float: Float = input_float.parse().unwrap();
+            assert_eq!(float, -23.123412);
+        }
+
+
+        let input_non_float = "42";
+
+        if FLOAT_RE.is_match(input_non_float) {
+            panic!("input does match a float, but shouldn't");
+        }
+    }
+
+    #[test]
+    fn integer_regex() {
+        let input_integer = "23";
+
+        if !INTEGER_RE.is_match(input_integer) {
+            panic!("input doesn't match an integer");
+        } else {
+            let integer: Integer = input_integer.parse().unwrap();
+            assert_eq!(integer, 23);
+        }
+
+        let input_integer = "-23";
+
+        if !INTEGER_RE.is_match(input_integer) {
+            panic!("input doesn't match an integer");
+        } else {
+            let integer: Integer = input_integer.parse().unwrap();
+            assert_eq!(integer, -23);
+        }
+
+
+        let input_non_integer = "42.76543";
+
+        if INTEGER_RE.is_match(input_non_integer) {
+            panic!("input does match an integer, but shouldn't");
+        }
+    }
+
+    #[test]
+    fn color_regex() {
+        let input_colors = vec![
+            "#ffffff",
+            "#deafff",
+            "#123456",
+            "#AB84E3",
+            "#e732e8",
+            "#975677",
+        ];
+        let output_values = vec![0xffffff, 0xdeafff, 0x123456, 0xAB84E3, 0xe732e8, 0x975677];
+
+        for i in 0..input_colors.len() {
+            let current_color = input_colors[i].clone();
+            let current_value = output_values[i].clone();
+
+            if !COLOR_RE.is_match(current_color) {
+                panic!("input doesn't match a color");
+            } else {
+                let color = COLOR_RE.captures_iter(current_color).next().unwrap();
+                let uint: u32 = u32::from_str_radix(&color[1], 16).unwrap();
+                assert_eq!(uint, current_value);
+            }
+        }
+    }
+
+    #[test]
+    fn char_regex() {
+        let input_chars = vec![
+            "'a'",
+            "'b'",
+            "'c'",
+            "'d'",
+            "'e'",
+            "'f'",
+            "'g'",
+            "'h'",
+            "'i'",
+        ];
+        let output_chars = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+
+        for i in 0..input_chars.len() {
+            let current_char = input_chars[i].clone();
+            let current_value = output_chars[i].clone();
+
+            if !CHAR_RE.is_match(current_char) {
+                panic!("input doesn't match a char");
+            } else {
+                let character = CHAR_RE.captures_iter(current_char).next().unwrap();
+                let real_char: char = character[1].chars().next().unwrap();
+                assert_eq!(real_char, current_value);
+            }
+        }
+    }
 
     #[test]
     fn comparison() {
